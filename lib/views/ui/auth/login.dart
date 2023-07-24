7c126d00b4fae3b16177496796380530609d0fcc
Moamen-Ashour <m.ashour.eng@gmail.com>
2023-07-24 10:57:30 +0200

    1- add constants 2- add app_bar widget 3- add Custom Text Field Widget 4- add login page with email textfiled 5- finish on boarding screen

--- lib/views/ui/auth/login.dart
+++ lib/views/ui/auth/login.dart
@@ -1,4 +1,13 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:jobhub/constants/app_constants.dart';
+import 'package:jobhub/views/common/app_bar.dart';
+import 'package:jobhub/views/common/app_style.dart';
+import 'package:jobhub/views/common/custom_textfield.dart';
+import 'package:jobhub/views/common/height_spacer.dart';
+import 'package:jobhub/views/common/reusable_text.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -8,7 +17,6 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-
   final TextEditingController email = TextEditingController();
   final TextEditingController password = TextEditingController();
 
@@ -18,8 +26,59 @@ class _LoginPageState extends State<LoginPage> {
     password.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(50),
+          child: CustomAppBar(
+            child: GestureDetector(
+              onTap: () {
+                Get.back;
+              },
+              child: Icon(CupertinoIcons.arrow_left),
+            ),
+            text: "Login",
+          ),
+        ),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const HeightSpacer(size: 50),
+              ReusableText(
+                text: "Welcom Back!",
+                style: appstyle(
+                  30,
+                  Color(kDark.value),
+                  FontWeight.w600,
+                ),
+              ),
+              ReusableText(
+                text: "fill the details to login to your account",
+                style: appstyle(
+                  16,
+                  Color(kDarkGrey.value),
+                  FontWeight.w600,
+                ),
+              ),
+              const HeightSpacer(size: 50),
+              CustomTextField(
+                controller: email,
+                keyboardType: TextInputType.emailAddress,
+                hintText: "Email",
+                validator: (email) {
+                  if (email!.isEmail || !email.contains("@")) {
+                    return "Please write a valid Email"
+                  }else{
+                    return null;
+                  }
+                },
+              )
+            ],
+          ),
+        ));
   }
 }
